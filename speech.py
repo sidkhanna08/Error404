b7104151bf3b7e4f964cd62863e3df98d6f542a5
sidkhanna08 <92032525+sidkhanna08@users.noreply.github.com>
2023-01-15 03:46:47 +0530

    Update speech.py

--- speech.py
+++ speech.py
@@ -9,66 +9,72 @@ import subprocess
 
 recorder = sr.Recognizer()
 engine = pyttsx3.init()
-voices =engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id,'rate',150)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
 
 def talk(text):
+
     engine.say(text)
     engine.runAndWait()
 
-flag=0
 def take_cmd():
     try:
         with sr.Microphone() as source:
+
             audio = recorder.listen(source)
             audio_cmd = recorder.recognize_google(audio)
             audio_cmd = audio_cmd.lower()
             audio_cmd1 = audio_cmd
-            if (audio_cmd=='hey siri'):
-                talk("ummm hmmm.....")
-                print("ummm hmmm.....")
-            elif 'hey siri' in audio_cmd:
-                talk(audio_cmd1.replace("hey siri",""))
+            if (audio_cmd=='hi melody'):
+                talk('ummm hmmm.....')
+                print('ummm hmmm.....')
+
+            elif 'hi melody' in audio_cmd:
+                talk(audio_cmd1.replace('hi melody',''))
                 print(audio_cmd1)
-                flag=1
-            elif (audio_cmd=='hey siri i love you'):
-                talk("i love you too sid")
-                print("i love you too sid")
+
+            elif (audio_cmd=='hi melody i love you'):
+                talk('i love you too sid')
+                print('i love you too sid')
 
     except:
         pass
+
     return audio_cmd
 
-def run_siri():
-    if (flag==1):
-        command = take_cmd()
-        print(command)
-        if 'play' in command:
-            song = command.replace('play','')
-            talk('playing' + song)
+def main():
+    command = take_cmd()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play','')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
 
-            pywhatkit.playonyt(song)
-        elif 'time' in command:
-            time = datetime.datetime.now().strftime('%I:%M %p')
-            print(time)
-            talk('Current time is ' + time )
-        elif 'joke' in command:
-            talk(pyjokes.get_joke())
-        elif 'what' in command:
-            info = wikipedia.summary(command,2)
-            talk(info)
-            pywhatkit.search(command)
-        elif 'open calculator' in command:
-            command= command.replace('open',"")
-            subprocess.Popen("C:\Windows\System32\calc.exe")
-        elif 'open notepad' in command:
-            command=command.replace('open notepad and type',"")
-            # subprocess.Popen("C:\Windows\System32\notepad.exe")
-            with open('new.txt','w') as g:
-                g.write(command)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('Current time is ' + time )
 
-        else :
-            talk('Please say the command again')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
 
-while True:
-    run_siri()
+    elif 'what' in command:
+        info = wikipedia.summary(command,2)
+        talk(info)
+        pywhatkit.search(command)
+
+    elif 'open calculator' in command:
+        command= command.replace('open',"")
+        subprocess.Popen("C:\Windows\System32\calc.exe")
+
+    elif 'open notepad' in command:
+        command=command.replace('open notepad and type',"")
+        # subprocess.Popen("C:\Windows\System32\notepad.exe")
+        with open('new.txt','w') as g:
+            g.write(command)
+
+    else:
+        talk('how can i help you')
+
+main()
+
