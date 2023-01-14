ed323887437251766babb3dd48e94f20ceb1b974
rudrasurana1025 <98961938+rudrasurana1025@users.noreply.github.com>
2023-01-15 03:17:56 +0530

    Update zoom.py

--- zoom.py
+++ zoom.py
@@ -1,48 +1,49 @@
-import cv2 as cv
-from handTrackingmodule import handDetector
+import cv2
+from handTrackingmodule1 import handDetector
 
-cap = cv.VideoCapture(0)
+cap = cv2.VideoCapture(0)
+
 cap.set(3,1280)
 cap.set(4,720)
 
-detector = handDetector()
+detector2 = handDetector()
 initialDist = None
 diff = 0
-cx,cy = 250, 250
+cx1,cy1 = 250, 250
 
 while True:
     success, frm = cap.read()
-    hands, frm = detector.findHands(frm)
-    frm1 = cv.imread("frm1.jpg")
+    hands, frm = detector2.findHands(frm)
+    # frm = cv2.imread("frm.jpg")
 
-    # frm = cv.flip(frm, 1)
-
+    # frm = cv2.flip(frm, 1)
+    # frm=cv2.flip(frm,1)
     if len(hands) == 2:
-        print(detector.fingersUp(hands[0]),detector.fingersUp(hands[1]))
-        if detector.fingersUp(hands[0]) == [1, 1, 0, 0, 0] and\
-                detector.fingersUp(hands[1]) == [1, 1, 0, 0, 0]:
+        # print(detector2.fingersUp(hands[0]),detector2.fingersUp(hands[1]))
+        if detector2.fingersUp(hands[0]) == [1, 1, 0, 0, 0] and\
+                detector2.fingersUp(hands[1]) == [1, 1, 0, 0, 0]:
 
             lmList1 = hands[0]["lmList"]
             lmList2 = hands[1]["lmList"]
             if initialDist is None:
-                length, info, frm = detector.findDistance((lmList1[8]),(lmList2[8]),frm)
+                length, info, frm = detector2.findDistance((lmList1[8]),(lmList2[8]),frm)
                 initialDist = length
 
-            length, info, frm = detector.findDistance(lmList1[8], lmList2[8], frm)
-            print(length)
+            length, info, frm = detector2.findDistance(lmList1[8], lmList2[8], frm)
+            # print(length)
             diff = int((length - initialDist)//2)
-            cx,cy = info[4:]
+            cx1,cy1 = info[4:]
     else:
         initialDist = None
 
     try:
-        h1, w1, _=frm1.shape
+        h1, w1, _=frm.shape
         new_h,new_w = ((h1+diff)//2)*2, ((w1+diff)//2)*2
-        frm1 =cv.resize(frm1,(new_w,new_h))
-        frm[cy-new_h//2 : cy+new_h//2 , cx-new_w//2 : cx+new_w//2] = frm1
+        frm =cv2.resize(frm,(new_w,new_h))
+        frm[cy1-new_h//2 : cy1+new_h//2 , cx1-new_w//2 : cx1+new_w//2] = frm
     except:
         pass
 
 
-    cv.imshow("Image", frm)
-    cv.waitKey(1)
+    cv2.imshow("Image", frm)
+    cv2.waitKey(1)
